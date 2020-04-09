8b22f5ec577d26a1b4ccddf661b0828d2ac20542
xamuel98 <samuelakintomiwa98@gmail.com>
2020-04-09 16:07:51 +0100

    Initial commit

--- djangoherokuapp/settings.py
+++ djangoherokuapp/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
